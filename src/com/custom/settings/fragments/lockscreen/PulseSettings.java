HertzifyOS/android_packages_apps_Settings

Settings: Add matrix pulse renderer style [2/2]
Change-Id: I1ce14866de2d6e39442c3c99f327dfe50e503e00
Signed-off-by: Ghosuto <clash.raja10@gmail.com>
Signed-off-by: HDzungx <hdzungx@gmail.com>

src/com/android/settings/display/PulseSettings.java
@@ -31,13 +31,51 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class PulseSettings extends SettingsPreferenceFragment {
+import com.hertzify.settings.preferences.SecureSettingListPreference;
+
+public class PulseSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_PULSE_RENDERER = "pulse_renderer";
+    private static final String KEY_PULSE_COLOR = "pulse_color";
+
+    private SecureSettingListPreference mPulseRenderer;
+    private SecureSettingListPreference mPulseColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.pulse_settings);
+
+        mPulseRenderer = (SecureSettingListPreference) findPreference(KEY_PULSE_RENDERER);
+        mPulseColor = (SecureSettingListPreference) findPreference(KEY_PULSE_COLOR);
+
+        if (mPulseRenderer != null) {
+            mPulseRenderer.setOnPreferenceChangeListener(this);
+            String currentRenderer = Settings.Secure.getStringForUser(
+                    getContentResolver(),
+                    Settings.Secure.PULSE_RENDERER,
+                    UserHandle.USER_CURRENT);
+            updateColorPreferenceVisibility(currentRenderer);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPulseRenderer) {
+            String value = (String) newValue;
+            updateColorPreferenceVisibility(value);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateColorPreferenceVisibility(String rendererValue) {
+        if (mPulseColor != null) {
+            boolean isMatrix = "matrix".equals(rendererValue);
+            mPulseColor.setVisible(!isMatrix);
+        }
     }
 
     public static void reset(Context context) {

AyakaUI/android_packages_apps_AyakaUISettings

Signed-off-by: whyakari <barryofc11@gmail.com>

src/com/custom/settings/fragments/lockscreen/PulseSettings.java
@@ -31,13 +31,51 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class PulseSettings extends SettingsPreferenceFragment {
+import com.custom.settings.preferences.SecureSettingListPreference;
+
+public class PulseSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_PULSE_RENDERER = "pulse_renderer";
+    private static final String KEY_PULSE_COLOR = "pulse_color";
+
+    private SecureSettingListPreference mPulseRenderer;
+    private SecureSettingListPreference mPulseColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.pulse_settings);
+
+        mPulseRenderer = (SecureSettingListPreference) findPreference(KEY_PULSE_RENDERER);
+        mPulseColor = (SecureSettingListPreference) findPreference(KEY_PULSE_COLOR);
+
+        if (mPulseRenderer != null) {
+            mPulseRenderer.setOnPreferenceChangeListener(this);
+            String currentRenderer = Settings.Secure.getStringForUser(
+                    getContentResolver(),
+                    Settings.Secure.PULSE_RENDERER,
+                    UserHandle.USER_CURRENT);
+            updateColorPreferenceVisibility(currentRenderer);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPulseRenderer) {
+            String value = (String) newValue;
+            updateColorPreferenceVisibility(value);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateColorPreferenceVisibility(String rendererValue) {
+        if (mPulseColor != null) {
+            boolean isMatrix = "matrix".equals(rendererValue);
+            mPulseColor.setVisible(!isMatrix);
+        }
     }
 
     public static void reset(Context context) {