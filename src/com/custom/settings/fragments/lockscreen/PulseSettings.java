HertzifyOS/android_packages_apps_Settings

Settings: Add pulse visualizer [2/2]
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>"

Change-Id: Ifbd9d65d046cd2534a6643c65dedb25c2722a08b
Signed-off-by: HDzungx <hdzungx@gmail.com>

src/com/android/settings/display/PulseSettings.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2016-2025 Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreferenceCompat;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PulseSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pulse_settings);
+    }
+
+    public static void reset(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.LOCKSCREEN_PULSE_ENABLED, 0, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.AMBIENT_PULSE_ENABLED, 1, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.PULSE_BAR_COUNT, 32, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.PULSE_ROUNDED_BARS, 0, UserHandle.USER_CURRENT);
+        Settings.Secure.putStringForUser(resolver,
+                Settings.Secure.PULSE_COLOR, "lavalamp", UserHandle.USER_CURRENT);
+        Settings.Secure.putStringForUser(resolver,
+                Settings.Secure.PULSE_RENDERER, "solid", UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HERTZIFY;
+    }
+}

AyakaUI/android_packages_apps_AyakaUISettings

Signed-off-by: whyakari <barryofc11@gmail.com>

src/com/custom/settings/fragments/lockscreen/PulseSettings.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2016-2025 Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.custom.settings.fragments.lockscreen;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreferenceCompat;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PulseSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pulse_settings);
+    }
+
+    public static void reset(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.LOCKSCREEN_PULSE_ENABLED, 0, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.AMBIENT_PULSE_ENABLED, 1, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.PULSE_BAR_COUNT, 32, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.PULSE_ROUNDED_BARS, 0, UserHandle.USER_CURRENT);
+        Settings.Secure.putStringForUser(resolver,
+                Settings.Secure.PULSE_COLOR, "lavalamp", UserHandle.USER_CURRENT);
+        Settings.Secure.putStringForUser(resolver,
+                Settings.Secure.PULSE_RENDERER, "solid", UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM;
+    }
+}